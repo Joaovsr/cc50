faef5ff3557b93c59a9e3aa59ed9cd7d23e1e5a1
Joao Vinicius <joaovinicius2525@gmail.com>
2024-01-21 21:11:52 -0300

    fifteen

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CC50
 Curso de ciência da computação de Havard.
+
+install ubuntu
+$ sudo apt-get install build-essential
+
+
+$ curl -s https://packagecloud.io/install/repositories/cs50/repo/script.deb.sh | sudo bash
+$ sudo apt install libcs50
--- hacker3/fifteen/fifteen.c
+++ hacker3/fifteen/fifteen.c
@@ -17,11 +17,12 @@
  
 #define _XOPEN_SOURCE 500
 
-#include <cc50.h>
+#include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <time.h>
+#include <string.h>
 
 // constants
 #define DIM_MIN 3
@@ -43,6 +44,16 @@ void draw(void);
 bool move(int tile);
 bool won(void);
 
+// GOD MODE
+void god_mode(void);
+
+// possible movements
+int possibilites[4];
+int len = 0;
+// blank space coordinates
+int x, y = 0; 
+// function to update possibilites
+void check_possibilites();
 
 int
 main(int argc, char *argv[])
@@ -84,10 +95,15 @@ main(int argc, char *argv[])
             printf("ftw!\n");
             break;
         }
-
+		
         // prompt for move
-        printf("Tile to move: ");
-        int tile = GetInt();
+        int tile = 0;
+		string input = get_string("Tile to move:");
+		if(strcmp(input,"GOD")==0){
+			god_mode();
+			break;
+		}else
+			tile = atoi(input);
 
         // move if possible, else report illegality
         if (!move(tile))
@@ -138,7 +154,32 @@ greet(void)
 void
 init(void)
 {
-    // TODO
+    // define a counter and populate the array
+    int c = 1;
+    for (int i = 0; i < d; i++) {
+        for (int j = 0; j < d; j++){
+	    // fill the tiles in descending order
+	    if (c == d * d)
+	        c = 0;
+	    
+	    board[i][j] = c;
+	    c++;
+	}
+    }
+    
+	srand((unsigned int)time(NULL));
+    int difficulty = rand() % 81 + 20; 
+	
+	x = d - 1;
+	y = d - 1;	
+	check_possibilites();
+	
+	for (int i = 0; i < difficulty; i++){
+		int r = rand() % len;
+		int tile_to_move = possibilites[r];
+		printf("%d ",move(tile_to_move));
+	}
+	
 }
 
 
@@ -149,7 +190,16 @@ init(void)
 void
 draw(void)
 {
-    // TODO
+	clear();
+    for (int i = 0; i < d; i++){
+        for (int j=0; j < d; j++){
+	    if (board[i][j] == 0)
+	        printf("[_]");
+	    else
+		printf("%2d ", board[i][j]);
+	}
+	printf("\n");
+    }
 }
 
 
@@ -160,9 +210,35 @@ draw(void)
 
 bool
 move(int tile)
-{
-    // TODO
-    return false;
+{	
+	// verify if is possible to make the move
+	bool possible = false;
+    for(int i=0; i<len; i++)
+		if (possibilites[i] == tile)
+			possible = true;
+
+	// if not possible return false
+	if (!possible)
+		return false;
+	
+	int x_temp = 0;
+	int y_temp = 0;
+	for (int i = 0; i < d; i++) {
+		for (int j = 0; j < d; j++){
+			if (board[i][j] == tile){
+				board[i][j] = 0;
+				x_temp = i;
+				y_temp = j;
+			}
+		}
+	}
+
+	board[x][y] = tile;
+	x = x_temp;
+	y = y_temp;
+
+	check_possibilites();
+    return true;
 }
 
 
@@ -174,6 +250,103 @@ move(int tile)
 bool
 won(void)
 {
-    // TODO
-    return false;
+    int n = 1;
+
+	// check to see if last tile is blank and return false if it is not
+	if(board[d-1][d-1] != 0)
+		return false;
+
+	// set up for loops to check if number are in ascending order beginning with 1
+	for (int i = 0; i < d; i++){
+		for (int j = 0; j < d; j++){
+			// check last grid position first, if blank, return true
+			if (i == d - 1 && j == d - 1)
+				return true;
+
+			// check the numbers on the rest of the tiles
+			if (n != board[i][j])
+				return false;
+			n++;
+		}
+	}
+	return true;
 }
+
+/*
+ * Check possibilites and update the array possibilites
+ */
+
+void
+check_possibilites(void){
+	len = 0;
+		
+	if (x == 0)
+		possibilites[len++] = board[1][y];
+	else if (x == d-1)
+		possibilites[len++] = board[x-1][y];
+	else {
+		possibilites[len++] = board[x-1][y];
+		possibilites[len++] = board[x+1][y];
+	}
+		
+	if (y == 0)
+		possibilites[len++] = board[x][1];
+	else if (y == d-1)
+		possibilites[len++] = board[x][y-1];
+	else {
+		possibilites[len++] = board[x][y-1];
+		possibilites[len++] = board[x][y+1];
+	}
+}
+
+void 
+god_mode(void){
+	//int row = 0;
+	while(true){
+					
+		int n = 1;
+		for (int i = 0; i < d; i++){
+			for (int j = 0; j < d; j++){
+				// check the numbers on the rest of the tiles
+				if (n != board[i][j]){
+					//row = (n-1) / d;
+					//printf("%d", row);
+					//printf("%d", n);
+					//get_int(" ");
+					goto leave;
+				}
+				n++;
+
+			}
+		}
+		
+		leave:
+
+		int a_len = 0;
+		int a_poss[4];
+		for (int i = 0; i < len; i++){
+			if(possibilites[i] > n-1)
+				a_poss[a_len++] = possibilites[i];
+		}
+
+		int r = rand() % a_len;
+		int tile = a_poss[r];
+		move(tile);
+
+		// clear the screen
+		clear();
+
+		// draw the current state of the board
+		draw();
+
+		// check for win
+		if (won())
+		{
+			printf("ftw!\n");
+			break;
+		}
+
+		usleep(500);
+	}
+}
+
